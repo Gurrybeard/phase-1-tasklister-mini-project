68120276e0e6c51951bdeaae1daab23c30411bed
Benjamin <bboksner@gmail.com>
2022-02-15 19:15:09 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener("click", function(event){
+    event.preventDefault()
+  });
+  
+  function text(){
+   let text = document.getElementById('new-task-description').value;
+   let ul = document.getElementById('tasks');
+   let li = document.createElement('li');
+   ul.appendChild(li).innerHTML=text;
+  }
+  document.querySelector('#submit').addEventListener("click",text);
+
 });
